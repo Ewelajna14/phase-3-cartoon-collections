598b71709c22220b83c5b9dad88e814c59b174c6
Ewelina Kowalczyk <ekowalczyk1994@gmail.com>
2021-12-28 18:35:39 -0600

    commit tree f4af2f445ee1df45fa2d441c47b43068025dc8c2

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,42 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+
+array_new = ["Doc", "Dopey", "Bashful", "Grumpy"]
+
+def roll_call_dwarves(array_new)
+  array_new.map.with_index do |dwarv, index|
+    index +=1  
+    puts "#{index}. #{dwarv}"  
+    end   
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map do |element|
+  element.capitalize + "!"  
+  end  
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+def long_planeteer_calls(words)
+  if words.any? {|element| element.length > 4}
+  true
+else
+  false
+end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(snack)
   cheese_types = ["cheddar", "gouda", "camembert"]
-end
+  snack.find do |cheese| 
+  if cheese.include?("cheddar")
+    "cheddar"
+   elsif cheese.include?("gouda")
+    "gouda"
+   elsif cheese.include?("camembert")
+    "camembert"
+   else
+     nil
+   end
+  end
+ end
